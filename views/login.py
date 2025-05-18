cc9b9ff0d6557d58a1de56d16a2bd098080a5843
MajidHashemi <majid2022hashemi@gmail.com>
2025-05-18 19:13:14 +0330

    final 23-class

--- views/login.py
+++ views/login.py
@@ -26,24 +26,56 @@ class LoginWindow:
         heading.place(x=100, y=5)
         
 
-        self.email_entry = Entry(frame, width=25, fg='black',border=0, bg='white',font=('Microsoft YaHeI UI Light',11))
-        self.email_entry .place(x=30,y=80)
-        self.email_entry .insert(0,"Email or phone")
+        # self.email_entry = Entry(frame, width=25, fg='black',border=0, bg='white',font=('Microsoft YaHeI UI Light',11))
+        # self.email_entry .place(x=30,y=80)
+        # self.email_entry .insert(0,"Email or phone")
 
-        Frame(frame,width=295, height=2, bg='black').place(x=25,y=107)
+
+
+        def on_entry_click(event):
+            if self.email_entry.get() == "Email or phone":
+                self.email_entry.delete(0, "end")  # delete all the text in the entry
+                self.email_entry.config(fg='black')  # change text color to black
+
+        def on_focusout(event):
+            if self.email_entry.get() == "":
+                self.email_entry.insert(0, "Email or phone")
+                self.email_entry.config(fg='grey')  # change text color to grey
+
+       # Create your entry widget
+        self.email_entry = Entry(frame, width=25, fg='grey', border=0, bg='white', 
+                                font=('Microsoft YaHeI UI Light', 11))
+        self.email_entry.insert(0, "Email or phone")
+        self.email_entry.place(x=30, y=80)
+
+        # Add bindings for focus in/out
+        self.email_entry.bind('<FocusIn>', on_entry_click)
+        self.email_entry.bind('<FocusOut>', on_focusout)
+
+      
+
+        
+
+        Frame(frame, width=250, height=2, bg='black').place(x=30, y=107)
+
 
 
         self.password_entry = Entry(frame, width=25, fg='black',border=0, bg='white',show='*' ,font=('Microsoft YaHeI UI Light',11))
         self.password_entry.place(x=30,y=150)
         self.password_entry.insert(0,"Password")
 
-        Frame(frame,width=295, height=2, bg='black').place(x=25,y=177)
+        Frame(frame,width=260, height=2, bg='black').place(x=25,y=177)
 
 
-        Button(frame,width=6,text='Sign in',border=0,bg='#57a1f8',command=self.login).place(x=35, y=204)
+        # Button(frame,width=6,text='Sign in',border=0,bg='#57a1f8',command=self.login).place(x=35, y=204)
+
+        next_button = Button(frame, width=15, pady=7, text='Sign in', bg='#1a73e8', fg='white', border=0, command=self.login,
+                    font=('Microsoft YaHeI UI Light', 11, 'bold'))
+        next_button.place(x=35, y=204)
+
         lable = Label(frame,text="Don't have an account?",fg='black',bg='white',font=('Microsoft YaHei UI Light',9))
         lable.place(x=75,y=270)
-        register = Button(frame,width=6,text='Create Account',border=0,bg='white',cursor='hand2',fg='#57a1f8',command=self.open_register)
+        register = Button(frame,width=12,text='Create Account',border=0,bg='white',cursor='hand2',fg='#57a1f8',command=self.open_register)
         register.place(x=215, y=270)  
 
     def login(self):
